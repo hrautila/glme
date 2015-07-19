d6272d9d2270e819a962a18003c4f872effb3976
Harri Rautila <harri.rautila@gmail.com>
2015-07-20 00:36:36 +0300

    New functions for structure encoding/decoding.

--- src/decoder.c
+++ src/decoder.c
@@ -9,8 +9,38 @@
 #include "gobber.h"
 #include "glme.h"
 
+static inline
+int __peek_base_type(glme_buf_t *dec, int id)
+{
+  unsigned int u = (id << 1);
+  if (dec->current >= dec->count)
+    return -1;
+  return (dec->buf[dec->current] == (char)u) ? 1 : -1;
+}
 
-int glme_decode_uint64(glme_buf_t *dec, uint64_t *v)
+static inline
+int __read_base_type(glme_buf_t *dec, int *id)
+{
+  if (dec->current >= dec->count)
+    return -1;
+  unsigned int u = dec->buf[dec->current];
+  *id = u & 0x1 ? -((int)(u >> 1)) : (int)(u >> 1);
+  return 1;
+}
+
+static inline
+int __decode_base_type(glme_buf_t *dec, int id)
+{
+  unsigned int u = (id << 1);
+  if (dec->current >= dec->count)
+    return -1;
+  return (dec->buf[dec->current++] == (char)u) ? 1 : -1;
+}
+
+// ----------------------------------------------------------------
+// Base type value decoding functions
+
+int glme_decode_value_uint64(glme_buf_t *dec, uint64_t *v)
 {
   int n;
 
@@ -23,7 +53,7 @@ int glme_decode_uint64(glme_buf_t *dec, uint64_t *v)
   return n;
 }
 
-int glme_decode_uint64_peek(glme_buf_t *dec, uint64_t *v)
+int glme_decode_peek_uint64(glme_buf_t *dec, uint64_t *v)
 {
   int n;
 
@@ -35,7 +65,7 @@ int glme_decode_uint64_peek(glme_buf_t *dec, uint64_t *v)
   return n;
 }
 
-int glme_decode_int64(glme_buf_t *dec, int64_t *v)
+int glme_decode_value_int64(glme_buf_t *dec, int64_t *v)
 {
   int n;
   n = gob_decode_int64(v, &dec->buf[dec->current], dec->count-dec->current); 
@@ -47,7 +77,7 @@ int glme_decode_int64(glme_buf_t *dec, int64_t *v)
   return n;
 }
 
-int glme_decode_int64_peek(glme_buf_t *dec, int64_t *v)
+int glme_decode_peek_int64(glme_buf_t *dec, int64_t *v)
 {
   int n;
   n = gob_decode_int64(v, &dec->buf[dec->current], dec->count-dec->current); 
@@ -58,43 +88,61 @@ int glme_decode_int64_peek(glme_buf_t *dec, int64_t *v)
   return n;
 }
 
-int glme_decode_ulong(glme_buf_t *dec, unsigned long *u)
+int glme_decode_value_uint32(glme_buf_t *dec, uint32_t *u)
 {
   int n;
   uint64_t u64;
-  n = glme_decode_uint64(dec, &u64);
+  n = glme_decode_value_uint64(dec, &u64);
+  *u = n < 0 ? 0 : (uint32_t)u64;
+  return n;
+}
+
+int glme_decode_value_int32(glme_buf_t *dec, int32_t *u)
+{
+  int n;
+  uint64_t u64;
+  n = glme_decode_value_int64(dec, &u64);
+  *u = n < 0 ? 0 : (int32_t)u64;
+  return n;
+}
+
+int glme_decode_value_ulong(glme_buf_t *dec, unsigned long *u)
+{
+  int n;
+  uint64_t u64;
+  n = glme_decode_value_uint64(dec, &u64);
   *u = n < 0 ? 0 : (unsigned long)u64;
   return n;
 }
 
-int glme_decode_long(glme_buf_t *dec, long *d)
+int glme_decode_value_long(glme_buf_t *dec, long *d)
 {
   int n;
   int64_t i64;
-  n = glme_decode_int64(dec, &i64);
+  n = glme_decode_value_int64(dec, &i64);
   *d = n < 0 ? 0 : (long)i64;
   return n;
 }
 
-int glme_decode_uint(glme_buf_t *dec, unsigned int *u)
+int glme_decode_value_uint(glme_buf_t *dec, unsigned int *u)
 {
   int n;
   uint64_t u64;
-  n = glme_decode_uint64(dec, &u64);
+  n = glme_decode_value_uint64(dec, &u64);
   *u = n < 0 ? 0 : (unsigned int)u64;
   return n;
 }
 
-int glme_decode_int(glme_buf_t *dec, int *d)
+int glme_decode_value_int(glme_buf_t *dec, int *d)
 {
   int n;
   int64_t i64;
-  n = glme_decode_int64(dec, &i64);
+  n = glme_decode_value_int64(dec, &i64);
   *d = n < 0 ? 0 : (int)i64;
   return n;
 }
 
-int glme_decode_double(glme_buf_t *dec, double *v)
+int glme_decode_value_double(glme_buf_t *dec, double *v)
 {
   int n;
   n = gob_decode_double(v, &dec->buf[dec->current], dec->count-dec->current); 
@@ -106,19 +154,113 @@ int glme_decode_double(glme_buf_t *dec, double *v)
   return n;
 }
 
-int glme_decode_float(glme_buf_t *dec, float *v)
+int glme_decode_value_float(glme_buf_t *dec, float *v)
 {
   int n;
   double dv;
-  n = glme_decode_double(dec, &dv);
+  n = glme_decode_value_double(dec, &dv);
   *v = n < 0 ? 0.0 : (float)dv;
   return n;
 }
 
-int glme_decode_vec(glme_buf_t *dec, void *s, size_t len)
+// ----------------------------------------------------------------
+// Base type decoding functions
+
+int glme_decode_uint64(glme_buf_t *dec, uint64_t *v)
+{
+  if (__decode_base_type(dec, GLME_UINT) < 0)
+    return -1;
+  int n = glme_decode_value_uint64(dec, v);
+  return n < 0 ? n : n+1;
+}
+
+int glme_decode_int64(glme_buf_t *dec, int64_t *v)
+{
+  if (__decode_base_type(dec, GLME_INT) < 0)
+    return -1;
+  int n = glme_decode_value_int64(dec, v);
+  return n < 0 ? n : n+1;
+}
+
+int glme_decode_uint32(glme_buf_t *dec, uint32_t *v)
+{
+  if (__decode_base_type(dec, GLME_UINT) < 0)
+    return -1;
+  int n = glme_decode_value_uint32(dec, v);
+  return n < 0 ? n : n+1;
+}
+
+int glme_decode_int32(glme_buf_t *dec, int32_t *v)
+{
+  if (__decode_base_type(dec, GLME_INT) < 0)
+    return -1;
+  int n = glme_decode_value_int32(dec, v);
+  return n < 0 ? n : n+1;
+}
+
+int glme_decode_ulong(glme_buf_t *dec, unsigned long *v)
+{
+  if (__decode_base_type(dec, GLME_UINT) < 0)
+    return -1;
+  int n = glme_decode_value_ulong(dec, v);
+  return n < 0 ? n : n+1;
+}
+
+int glme_decode_long(glme_buf_t *dec, long *v)
+{
+  if (__decode_base_type(dec, GLME_INT) < 0)
+    return -1;
+  int n = glme_decode_value_long(dec, v);
+  return n < 0 ? n : n+1;
+}
+
+int glme_decode_uint(glme_buf_t *dec, unsigned int *v)
+{
+  if (__decode_base_type(dec, GLME_UINT) < 0)
+    return -1;
+  int n = glme_decode_value_uint(dec, v);
+  return n < 0 ? n : n+1;
+}
+
+int glme_decode_int(glme_buf_t *dec, int *v)
+{
+  if (__decode_base_type(dec, GLME_INT) < 0)
+    return -1;
+  int n = glme_decode_value_int(dec, v);
+  return n < 0 ? n : n+1;
+}
+
+int glme_decode_double(glme_buf_t *dec, double *v)
+{
+  if (__decode_base_type(dec, GLME_FLOAT) < 0)
+    return -1;
+  int n = glme_decode_value_double(dec, v);
+  return n < 0 ? n : n+1;
+}
+
+int glme_decode_float(glme_buf_t *dec, float *v)
+{
+  if (__decode_base_type(dec, GLME_FLOAT) < 0)
+    return -1;
+  int n = glme_decode_value_float(dec, v);
+  return n < 0 ? n : n+1;
+}
+
+// ------------------------------------------------------------------
+// byte array and string functions
+
+int glme_decode_vector(glme_buf_t *dec, void *s, size_t len)
 {
   int n;
   int64_t dlen = 0;
+
+  // we accept BYTE arrays and STRINGs
+  if (__peek_base_type(dec, GLME_VECTOR) < 0 &&
+      __peek_base_type(dec, GLME_STRING) < 0 )
+    return -1;
+
+  dec->current++;
+
   n = gob_decode_uint64(&dlen, &dec->buf[dec->current], dec->count-dec->current);
   if (n > dec->count - dec->current) {
     // underflow
@@ -130,16 +272,20 @@ int glme_decode_vec(glme_buf_t *dec, void *s, size_t len)
   }
   // update current pointer;
   dec->current += n;
-  memcpy(s, &dec->buf[dec->current], len);
+  memcpy(s, &dec->buf[dec->current], (len < dlen ? len : dlen));
+  if (dlen < len)
+    memset(&((char *)s)[dlen], 0, len-dlen);
   dec->current += dlen;
-  return n + dlen;
+  return n + dlen + 1;
 }
 
-int glme_decode_bytes(glme_buf_t *dec, void **s)
+// Decodes byte array from buffer and allocate space for it.
+int glme_decode_bytes(glme_buf_t *dec, void **s, size_t len)
 {
   int n;
   int64_t dlen = 0;
   char *nb;
+
   n = gob_decode_uint64(&dlen, &dec->buf[dec->current], dec->count-dec->current);
   if (n > dec->count - dec->current) {
     // underflow
@@ -149,10 +295,19 @@ int glme_decode_bytes(glme_buf_t *dec, void **s)
     // underflow
     return -(dlen+n);
   }
-  nb = malloc(dlen);
-  if (nb) {
-    memcpy(nb, &dec->buf[dec->current+n], dlen);
-    *s = nb;
+  if (len == 0) {
+    nb = malloc(dlen);
+    if (nb) {
+      memcpy(nb, &dec->buf[dec->current+n], dlen);
+      *s = nb;
+    }
+  } else {
+    nb = (char *)(*s);
+    if (nb) {
+      memcpy(nb, &dec->buf[dec->current+n], (dlen < len ? dlen : len));
+      if (dlen < len)
+        memset(&nb[dlen], 0, len-dlen);
+    }
   }
   dec->current += n + dlen;
   return n + dlen;
@@ -160,7 +315,210 @@ int glme_decode_bytes(glme_buf_t *dec, void **s)
 
 int glme_decode_string(glme_buf_t *dec, char **s)
 {
-  return glme_decode_bytes(dec, (void **)s);
+  if (__decode_base_type(dec, GLME_STRING) < 0)
+    return -1;
+  *s = (char *)0;
+  int n = glme_decode_bytes(dec, (void **)s, 0);
+  return n < 0 ? n : n+1;
+}
+
+// ----------------------------------------------------------------
+// Type decoding functions
+
+int glme_decode_type(glme_buf_t *dec, int *t)
+{
+  int n;
+  int64_t i64 = 0;
+  n = glme_decode_value_int64(dec, &i64);
+  *t = (int)i64;
+  return n;
+}
+
+int glme_decode_peek_type(glme_buf_t *dec, int *typeid)
+{
+  int n;
+  int64_t i64;
+  n = glme_decode_peek_int64(dec, &i64);
+  if (n < 0)
+    return n;
+  *typeid = (int)i64;
+  return n;
+}
+
+// ----------------------------------------------------------------
+// Structure decoding functions
+
+
+int glme_decode_delta_test(glme_buf_t *dec, unsigned int delta)
+{
+  int n;
+  uint64_t offset;
+
+  // read offset at read pointer
+  n = gob_decode_uint64(&offset, &dec->buf[dec->current], dec->count - dec->current);
+  if (n > dec->count - dec->current)
+    return -n;
+
+  return delta == offset ? 1 : 0;
+}
+
+int glme_decode_field(glme_buf_t *dec, unsigned int *delta, int etype, int flags, 
+                      void *vptr, size_t *nlen, size_t esize, decoder dfunc)
+{
+  int n, elemtype, typeid;
+  uint64_t offset, alen, __at_start = dec->current;
+  void *nptr;
+
+  // read offset at read pointer
+  n = gob_decode_uint64(&offset, &dec->buf[dec->current], dec->count - dec->current);
+  if (n > dec->count - dec->current)
+    return -n;
+
+  if (offset == 0 || *delta == 0) {
+    // end of struct or we have already seen end of struct
+    *delta = 0;
+    return 0;
+  }
+
+  if (*delta < offset) {
+    // not yet
+    *delta += 1;
+    return 0;
+  }
+  // decode; set delta to 1, move read pointer and call decoder function
+  dec->current += n;
+  if (glme_decode_peek_type(dec, &typeid) < 0)
+    return -1;
+
+  if (typeid == GLME_ARRAY && (flags & GLME_F_ARRAY == 0)) {
+    // not expecting array
+    return -3;
+  }
+  if (etype != 0 && typeid != GLME_ARRAY && typeid != etype) {
+    // not this type
+    return -3;
+  }
+
+  switch (typeid) {
+  case GLME_ARRAY:
+    if ((n = glme_decode_array_start(dec, &typeid, &alen)) < 0)
+      return n;
+    if (etype != 0 && typeid != etype)
+      return -1;
+
+    // assume that we need to allocate array element space
+    nptr = (void *)0;
+    if (*nlen > 0) {
+      // if nlen > 0 expecting fixed size array; space provided
+      nptr = (void *)(*((uint64_t **)vptr));
+      // fixed size array must have enough space
+      if (*nlen < alen)
+        return -1;
+    }
+    if ((n = glme_decode_array_data(dec, &nptr, alen, esize, dfunc)) < 0)
+      return n;
+    if (*nlen == 0) {
+      // return allocated pointer and actual length;
+      *nlen = alen;
+      *((uint64_t **)vptr) = nptr;
+    }
+    break;
+
+  case GLME_STRING:
+    // variable string
+    n = glme_decode_string(dec, (char **)&nptr);
+    if (n < 0)
+      return -n;
+    *((uint64_t **)vptr) = nptr;
+    break;
+
+  case GLME_VECTOR:
+    // fixed size byte vector; esize is 1 and nlen is number of bytes
+    n = glme_decode_vector(dec, vptr, *nlen);
+    break;
+
+  case GLME_INT:
+  case GLME_UINT:
+  case GLME_FLOAT:
+  case GLME_COMPLEX:
+    n = (*dfunc)(dec, vptr);
+    break;
+
+  default:
+    if ((n = glme_decode_type(dec, &typeid)) < 0)
+      return n;
+    if (etype != 0 && etype != typeid)
+      return -3;
+    // embedded structure  or structure pointer
+    if (flags & GLME_F_PTR) {
+      // vptr is pointer to pointer (void **);
+      nptr = malloc(esize);
+      if (! nptr)
+        return -1;
+    } else {
+      nptr = vptr;
+    }
+    if ((n = (*dfunc)(dec, nptr)) < 0) {
+      // if we have allocated memory, release it.
+      if (flags & GLME_F_PTR)
+        free(nptr);
+      return n;
+    }
+    if (flags & GLME_F_PTR) {
+      // it was a struct pointer needing 
+      // return pointer to the memory block
+      *((uintptr_t **)vptr) = nptr;
+    }
+    break;
+  }
+  if (n > 0)
+    *delta = 1;
+  return n < 0 ? n : dec->current - __at_start;
+}
+
+
+int glme_decode_struct(glme_buf_t *dec, int typeid, void *dptr, decoder dfunc)
+{
+  uint64_t __at_start = dec->current;
+  int n, typ;
+
+  if (! dfunc)
+    return -1;
+
+  if (glme_decode_type(dec, &typ) < 0)
+    return -1;
+
+  // if specified type id is non-zero check decoded id.
+  if (typeid != 0 && typ != typeid)
+    return -3;
+
+  if ((n = (*dfunc)(dec, dptr) ) < 0)
+    return n;
+
+  return dec->current - __at_start;
+}
+
+int glme_decode_value_struct(glme_buf_t *dec, void *dptr, decoder dfunc)
+{
+  uint64_t __at_start = dec->current;
+  int n;
+
+  if (! dfunc)
+    return -1;
+
+  if ((n = (*dfunc)(dec, dptr) ) < 0)
+    return n;
+
+  return dec->current - __at_start;
+}
+
+
+int glme_decode_start_struct(glme_buf_t *dec, int *delta)
+{
+  if (!delta)
+    return -1;
+  *delta = 1;
+  return 0;
 }
 
 int glme_decode_end_struct(glme_buf_t *dec)
@@ -177,6 +535,100 @@ int glme_decode_end_struct(glme_buf_t *dec)
   dec->current += n;
   return n;
 }
+
+// ---------------------------------------------------------------------
+// Array decoding functions
+
+int glme_decode_array_start(glme_buf_t *dec, int *typeid, size_t *len)
+{
+  int n, nc = 0;
+  n = __decode_base_type(dec, GLME_ARRAY);
+  if (n < 0)
+    return n;
+  nc += n;
+  n = glme_decode_type(dec, typeid);
+  if (n < 0)
+    return n;
+  nc += n;
+  n = glme_decode_value_uint64(dec, len);
+  return n < 0 ? n : nc + n;
+}
+
+int glme_decode_array_header(glme_buf_t *dec, int *typeid, size_t *len)
+{
+  int n0, n1;
+  if ((n0 = glme_decode_type(dec, typeid)) < 0)
+    return -1;
+  if ((n1 = glme_decode_value_uint64(dec, len)) < 0)
+    return -1;
+  return n1 + n0;
+}
+
+int glme_decode_array_data(glme_buf_t *dec, void **dst,
+                           size_t len, size_t esize, decoder func)
+{
+  char *ptr = (char *)(*dst);
+  int k, n;
+  size_t i, __at_start = dec->current;
+
+  if (len == 0)
+    return 0;
+
+  if (! ptr) {
+    ptr = (char *)calloc(len, esize);
+    if (! ptr)
+      return -1; 
+    *(char **)dst = ptr;
+  }
+  for (k = 0, i = 0; k < len; k++, i += esize) {
+    if ((n = (*func)(dec, (void *)&ptr[i])) < 0)
+      return n;
+  }
+  return dec->current - __at_start;
+}
+
+// decode full array
+int glme_decode_array(glme_buf_t *dec, int *typeid, void **dst,
+                      size_t *len, size_t esize, decoder dfunc)
+{
+  size_t alen;
+  uint64_t __at_start = dec->current;
+
+  if (__decode_base_type(dec, GLME_ARRAY) < 0)
+    return -1;
+  
+  if (glme_decode_array_header(dec, typeid, &alen) < 0)
+    return -1;
+
+  if (*len > 0 && *len < alen)
+    return -1;
+  
+  if (glme_decode_array_data(dec, dst, alen, esize, dfunc) < 0)
+    return -1;
+  
+  return dec->current - __at_start;
+}
+
+
+// decode array value (Array sans ARRAY typeid)
+int glme_decode_value_array(glme_buf_t *dec, int *typeid, void **dst,
+                            size_t *len, size_t esize, decoder dfunc)
+{
+  size_t alen;
+  uint64_t __at_start = dec->current;
+
+  if (glme_decode_array_header(dec, typeid, &alen) < 0)
+    return -1;
+
+  if (*len > 0 && *len < alen)
+    return -1;
+  
+  if (glme_decode_array_data(dec, dst, alen, esize, dfunc) < 0)
+    return -1;
+  
+  return dec->current - __at_start;
+}
+
 
 // Local Variables:
 // indent-tabs-mode: nil
--- src/encoder.c
+++ src/encoder.c
@@ -2,7 +2,6 @@
 // Copyright (c) Harri Rautila, 2015
 
 #include <stdlib.h>
-#include "glme/gobber.h"
 
 #ifdef __INLINE__
 #undef __INLINE__
@@ -13,14 +12,34 @@
 #include "gobber.h"
 #include "glme.h"
 
+/*
+ * Encode basic type id (0 < id < 32) directly to buffer. 
+ */
+static inline
+int __encode_base_type(glme_buf_t *enc, int id)
+{
+  int rc = 0;
+  unsigned int u = (id << 1);
+  while (enc->buflen <= enc->count) {
+    if (rc) return -1;
+    rc++;
+    glme_buf_resize(enc, enc->buflen < 1024 ? enc->buflen : 1024);
+  }
+  enc->buf[enc->count] = (unsigned char)u;
+  enc->count += 1;
+  return 1;
+}
 
-int glme_encode_uint64(glme_buf_t *enc, uint64_t v)
+// -------------------------------------------------------------------------
+// Plain value encoding for base types
+
+int glme_encode_value_uint64(glme_buf_t *enc, const uint64_t *v)
 {
   int n, rc = 0;
   if (!enc)
     return 0;
  retry:
-  n = gob_encode_uint64(&enc->buf[enc->count], enc->buflen-enc->count, v);
+  n = gob_encode_uint64(&enc->buf[enc->count], enc->buflen-enc->count, *v);
   if (n > enc->buflen-enc->count) {
     if (rc > 1)
       return -1;
@@ -32,13 +51,13 @@ int glme_encode_uint64(glme_buf_t *enc, uint64_t v)
   return n;
 }
 
-int glme_encode_int64(glme_buf_t *enc, int64_t v)
+int glme_encode_value_int64(glme_buf_t *enc, const int64_t *v)
 {
   int n, rc = 0;
   if (!enc)
     return 0;
  retry:
-  n = gob_encode_int64(&enc->buf[enc->count], enc->buflen-enc->count, v);
+  n = gob_encode_int64(&enc->buf[enc->count], enc->buflen-enc->count, *v);
   if (n > enc->buflen-enc->count) {
     if (rc > 1)
       return -1;
@@ -50,13 +69,13 @@ int glme_encode_int64(glme_buf_t *enc, int64_t v)
   return n;
 }
 
-int glme_encode_double(glme_buf_t *enc, double v)
+int glme_encode_value_double(glme_buf_t *enc, const double *v)
 {
   int n, rc = 0;
   if (!enc)
     return 0;
  retry:
-  n = gob_encode_double(&enc->buf[enc->count], enc->buflen-enc->count, v);
+  n = gob_encode_double(&enc->buf[enc->count], enc->buflen-enc->count, *v);
   if (n > enc->buflen-enc->count) {
     if (rc > 1)
       return -1;
@@ -69,7 +88,123 @@ int glme_encode_double(glme_buf_t *enc, double v)
 }
 
 
-int glme_encode_bytes(glme_buf_t *enc, void *v, size_t vlen)
+int glme_encode_value_ulong(glme_buf_t *gbuf, const unsigned long *v)
+{
+  uint64_t u64 = (uint64_t)(*v);
+  return glme_encode_value_uint64(gbuf, &u64); //(uint64_t)v
+}
+
+int glme_encode_value_long(glme_buf_t *gbuf, const long *v)
+{
+  int64_t u64 = (int64_t)(*v);
+  return glme_encode_value_int64(gbuf, &u64); //(int64_t)v);
+}
+
+int glme_encode_value_uint(glme_buf_t *gbuf, const unsigned int *v)
+{
+  uint64_t u64 = (uint64_t)(*v);
+  return glme_encode_value_uint64(gbuf, &u64); //(uint64_t)v);
+}
+
+int glme_encode_value_int(glme_buf_t *gbuf, const int *v)
+{
+  int64_t u64 = (int64_t)(*v);
+  return glme_encode_value_int64(gbuf, &u64); //(int64_t)v);
+}
+
+int glme_encode_value_float(glme_buf_t *gbuf, const float *v)
+{
+  double d = (double)(*v);
+  return glme_encode_value_double(gbuf, &d); //(double)v);
+}
+
+// -------------------------------------------------------------------------
+// Base type encoding functions.
+
+int glme_encode_uint64(glme_buf_t *enc, const uint64_t *v)
+{
+  int n, nc;
+  n = __encode_base_type(enc, GLME_UINT);
+  if (n < 0)
+    return n;
+  nc = glme_encode_value_uint64(enc, v);
+  return nc < 0 ? nc : nc + n;
+}
+
+int glme_encode_int64(glme_buf_t *enc, const int64_t *v)
+{
+  int n, nc;
+  n = __encode_base_type(enc, GLME_INT);
+  if (n < 0)
+    return n;
+  nc = glme_encode_value_int64(enc, v);
+  return nc < 0 ? nc : nc + n;
+}
+
+int glme_encode_ulong(glme_buf_t *enc, const unsigned long *v)
+{
+  int n, nc;
+  n = __encode_base_type(enc, GLME_UINT);
+  if (n < 0)
+    return n;
+  nc = glme_encode_value_ulong(enc, v);
+  return nc < 0 ? nc : nc + n;
+}
+
+int glme_encode_long(glme_buf_t *enc, const long *v)
+{
+  int n, nc;
+  n = __encode_base_type(enc, GLME_INT);
+  if (n < 0)
+    return n;
+  nc = glme_encode_value_long(enc, v);
+  return nc < 0 ? nc : nc + n;
+}
+
+int glme_encode_uint(glme_buf_t *enc, const unsigned int *v)
+{
+  int n, nc;
+  n = __encode_base_type(enc, GLME_UINT);
+  if (n < 0)
+    return n;
+  nc = glme_encode_value_uint(enc, v);
+  return nc < 0 ? nc : nc + n;
+}
+
+int glme_encode_int(glme_buf_t *enc, const int *v)
+{
+  int n, nc;
+  n = __encode_base_type(enc, GLME_INT);
+  if (n < 0)
+    return n;
+  nc = glme_encode_value_int(enc, v);
+  return nc < 0 ? nc : nc + n;
+}
+
+int glme_encode_double(glme_buf_t *enc, const double *v)
+{
+  int n, nc;
+  n = __encode_base_type(enc, GLME_FLOAT);
+  if (n < 0)
+    return n;
+  nc = glme_encode_value_double(enc, v);
+  return nc < 0 ? nc : nc + n;
+}
+
+int glme_encode_float(glme_buf_t *enc, const float *v)
+{
+  int n, nc;
+  n = __encode_base_type(enc, GLME_FLOAT);
+  if (n < 0)
+    return n;
+  nc = glme_encode_value_float(enc, v);
+  return nc < 0 ? nc : nc + n;
+}
+
+// --------------------------------------------------------------------
+// Byte array types (vectors and strings)
+
+int glme_encode_bytes(glme_buf_t *enc, const void *v, size_t vlen)
 {
   int n, rc = 0;
   if (!enc)
@@ -92,60 +227,218 @@ int glme_encode_bytes(glme_buf_t *enc, void *v, size_t vlen)
       return -1;
     }
     glme_buf_resize(enc, vlen - (enc->buflen - enc->count));
+    rc++;
   }
   memcpy(&enc->buf[enc->count], v, vlen);
   enc->count += vlen;
   return n+vlen;
 }
 
-int glme_encode_ulong(glme_buf_t *gbuf, unsigned long v)
+int glme_encode_string(glme_buf_t *gbuf, const char *s)
 {
-  return glme_encode_uint64(gbuf, (uint64_t)v);
+  int n = __encode_base_type(gbuf, GLME_STRING);
+  if (n < 0)
+    return n;
+  return n + glme_encode_bytes(gbuf, s, strlen(s)+1);
 }
 
-int glme_encode_long(glme_buf_t *gbuf, long v)
+int glme_encode_vector(glme_buf_t *gbuf, const char *s, size_t len)
 {
-  return glme_encode_int64(gbuf, (int64_t)v);
+  int n = __encode_base_type(gbuf, GLME_VECTOR);
+  if (n < 0)
+    return n;
+  return n + glme_encode_bytes(gbuf, s, len);
 }
 
-int glme_encode_uint(glme_buf_t *gbuf, unsigned int v)
+// -------------------------------------------------------------------------
+// Array functions
+
+int glme_encode_array_header(glme_buf_t *gbuf, int typeid, size_t sz)
 {
-  return glme_encode_uint64(gbuf, (uint64_t)v);
+  int n1, n0; 
+
+  if ((n0 = glme_encode_type(gbuf, typeid)) < 0)
+    return n0;
+
+  if ((n1 = glme_encode_value_uint64(gbuf, &sz)) < 0)
+    return n1;
+  return n1 + n0;
 }
 
-int glme_encode_int(glme_buf_t *gbuf, int v)
+int glme_encode_array_start(glme_buf_t *enc, int typeid, size_t sz)
 {
-  return glme_encode_int64(gbuf, (int64_t)v);
+  int n0; 
+
+  if (__encode_base_type(enc, GLME_ARRAY) < 0)
+    return -1;
+
+  if ((n0 = glme_encode_array_header(enc, typeid, sz)) < 0)
+    return -1;
+
+  return 1 + n0;
 }
 
-int glme_encode_float(glme_buf_t *gbuf, float v)
+int glme_encode_array_data(glme_buf_t *enc, const void *vptr,
+                           size_t len, size_t esize, encoder efunc)
 {
-  return glme_encode_double(gbuf, (double)v);
+  const char *ptr = (const char *)vptr;
+  int k, n;
+  size_t i, __at_start = enc->count;
+
+  if (! efunc)
+    return -1;
+
+  for (k = 0, i = 0; k < len; k++, i += esize) {
+    if ((n = (*efunc)(enc, (const void *)&ptr[i])) < 0)
+      return n;
+  }
+  return enc->count - __at_start;
 }
 
-int glme_encode_string(glme_buf_t *gbuf, char *s)
+int glme_encode_value_array(glme_buf_t *enc, int typeid, const void *vptr, size_t len,
+                            size_t esize, encoder efunc)
 {
-  return glme_encode_bytes(gbuf, s, strlen(s)+1);
+  size_t __at_start = enc->count;
+
+  if (glme_encode_array_header(enc, typeid, len) < 0)
+    return -1;
+
+  if (len > 0) {
+    if (glme_encode_array_data(enc, vptr, len, esize, efunc) < 0)
+      return -1;
+  }
+  return enc->count - __at_start;
 }
 
-int glme_encode_start_array(glme_buf_t *gbuf, size_t sz)
+int glme_encode_array(glme_buf_t *enc, int typeid,
+                      const void *vptr, size_t len, size_t esize, encoder efunc)
 {
-  return glme_encode_uint64(gbuf, (uint64_t)sz);
+  size_t __at_start = enc->count;
+
+  if (__encode_base_type(enc, GLME_ARRAY) < 0)
+    return -1;
+
+  if (glme_encode_array_header(enc, typeid, len) < 0)
+    return -1;
+
+  if (vptr && len > 0) {
+    if (glme_encode_array_data(enc, vptr, len, esize, efunc) < 0)
+      return -1;
+  }
+  return enc->count - __at_start;
 }
 
-int glme_encode_end_array(glme_buf_t *gbuf, size_t sz)
+int glme_encode_type(glme_buf_t *gbuf, int typeid)
 {
+  int64_t __t;
+  if (typeid <= GLME_BASE_MAX) {
+    return __encode_base_type(gbuf, typeid);
+  }
+  __t = (int64_t)typeid;
+  return glme_encode_value_int64(gbuf, &__t);
+}
+
+// -------------------------------------------------------------------------
+// Structure functions
+
+
+int glme_encode_start_struct(glme_buf_t *gbuf, int *delta)
+{
+  if (!delta)
+    return -1;
+  *delta = 1;
   return 0;
-}
-
-int glme_encode_start_struct(glme_buf_t *gbuf, int typeid)
-{
-  return glme_encode_int64(gbuf, (int64_t)typeid);
 }
 
 int glme_encode_end_struct(glme_buf_t *gbuf)
 {
-  return glme_encode_uint64(gbuf, (uint64_t)0);
+  uint64_t u64 = 0;
+  return glme_encode_value_uint64(gbuf, &u64);
+}
+
+int glme_encode_field(glme_buf_t *enc, int *delta, int typeid, int flags,
+                      const void *vptr, size_t nlen, size_t esize, encoder efunc)
+{
+  int n;
+  uint64_t __at_start = enc->count;
+
+  if (! vptr || (vptr && esize == 0)) {
+    // empty field; omit from stream and increment delta;
+    *delta += 1;
+    return 0;
+  }
+
+  if (vptr && (flags & GLME_F_ARRAY) && nlen == 0) {
+    // zero length array field is omitted
+    *delta += 1;
+    return 0;
+  }
+  
+  // zero length string is not encoded (is this good as other side is gets null pointer??)
+  if (typeid == GLME_STRING && vptr && (*((char *)vptr) == 0)) {
+    *delta += 1;
+    return 0;
+  }
+  // what about zero length byte vector?? should it be omitted too?
+
+  if (glme_encode_value_uint(enc, delta) < 0)
+    return -1;
+
+  // some simple type or structure
+  switch (typeid) {
+  case GLME_INT:
+  case GLME_UINT:
+  case GLME_FLOAT:
+    if (flags & GLME_F_ARRAY) {
+      // array of (int, uint, float)
+      n = glme_encode_array(enc, typeid, vptr, nlen, esize, efunc);
+    } else {
+      n = (*efunc)(enc, vptr);
+    }
+    break;
+
+  case GLME_VECTOR:
+    n = glme_encode_vector(enc, vptr, nlen);
+    break;
+      
+  case GLME_STRING:
+    n = glme_encode_string(enc, (char *)vptr);
+    break;
+
+  default:
+    if (flags & GLME_F_ARRAY) {
+      n = glme_encode_array(enc, typeid, vptr, nlen, esize, efunc);
+    } else {
+      if (glme_encode_type(enc, typeid) < 0)
+        return -1;
+      n = (*efunc)(enc, vptr);
+    }
+  }
+  if (n < 0)
+    return -1;
+  // set delta to one
+  *delta = 1;
+  return enc->count - __at_start;
+}
+
+int glme_encode_struct(glme_buf_t *enc, int typeid, const void *ptr, encoder efunc)
+{
+  int n;
+  uint64_t __at_start = enc->count;
+
+  // if null pointer then no data; only things pointed to are encoded
+  if (!ptr)
+    return 0;
+
+  if (glme_encode_type(enc, typeid) < 0)
+    return -1;
+
+  if (!efunc)
+    return -1;
+  if ((n = (*efunc)(enc, ptr)) < 0)
+    return n;
+
+  return enc->count - __at_start;
 }
 
 // Local Variables:
